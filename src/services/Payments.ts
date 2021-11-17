14d25cc4fda2de4e98a01871ac50d13c6bb13c0b
Alex Kehl <alexander.kehl@apprologic.de>
2021-11-17 01:02:20 +0100

    Adjust payments service to work with multiple ids

--- src/services/Payments.ts
+++ src/services/Payments.ts
@@ -1,37 +1,48 @@
 import Stripe from 'stripe';
 import HttpStatus from '../../common/constants/HttpStatus';
 import { BuyImageDto } from '../../common/interface/Dto';
-import { CLIENT_URL } from '../../config';
+import { withParallel } from '../../common/utils/Functions';
+import { CLIENT_URL, STRIPE_API_KEY } from '../../config';
 import { GalleryImage, GalleryImageDoc } from '../model/GalleryImage';
 import { tryToExecute } from '../utils/HttpErrors';
 import { makeHttpResponse } from '../utils/HttpResponses';
+import * as R from 'remeda';
 
-const stripe = new Stripe(
-  'sk_test_51JbOrxGTcbQZD9rEMtxJ6UTeyOvRyvlppVCkbG6fwIjDibIGaG0l88q4kTbgagvpJBW0q98UG4t8xJC3xXHQpEaw00CS5cxLO1',
-  { apiVersion: '2020-08-27' }
-);
+const stripe = new Stripe(STRIPE_API_KEY, { apiVersion: '2020-08-27' });
 
-const createStripeSession = async ({ price, id }: BuyImageDto) => {
-  const imageDoc = await tryToExecute<GalleryImageDoc>({
-    fnToTry: async () => GalleryImage.findOne({ id }),
+const createStripeSession = async ({ ids }: BuyImageDto) => {
+  const imageDocs = await tryToExecute<GalleryImageDoc[]>({
+    fnToTry: async () => GalleryImage.find({ id: { $in: ids } }),
     httpErrorData: {
       statusCode: HttpStatus.NOT_FOUND,
       data: { error: 'GalleryImage is not existing.' },
     },
   });
+  console.log(imageDocs);
 
-  const product = await stripe.products.create({
-    name: imageDoc.name,
-  });
+  const products = await withParallel({ threads: imageDocs.length })(
+    (imageDoc) => {
+      return stripe.products.create({
+        name: imageDoc.name,
+      });
+    }
+  )(imageDocs);
 
-  const priceObj = await stripe.prices.create({
-    product: product.id,
-    unit_amount: price * 100,
-    currency: 'eur',
-  });
+  const priceObjects = await withParallel({ threads: imageDocs.length })(
+    ([imageDoc, product]) => {
+      return stripe.prices.create({
+        product: product.id,
+        unit_amount: imageDoc.price! * 100,
+        currency: 'eur',
+      });
+    }
+  )(R.zip(imageDocs, products));
 
   const session = await stripe.checkout.sessions.create({
-    line_items: [{ price: priceObj.id, quantity: 1 }],
+    line_items: priceObjects.map((priceObj) => ({
+      price: priceObj.id,
+      quantity: 1,
+    })),
     payment_method_types: ['card', 'giropay', 'sofort'],
     mode: 'payment',
     success_url: `${CLIENT_URL}/gallery/acryl/`,
